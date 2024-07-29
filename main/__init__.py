48db201ba3a19c0212ef34dcf46006d81ccef98b
XXF91 <mrsjoaker@gmail.com>
2024-07-29 20:54:08 +0300

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -11,13 +11,7 @@ import logging, time, sys
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-# variables
-API_ID = config("API_ID", default=13384432, cast=int)
-API_HASH = config("API_HASH", default="ea9db4503ed7088b788e06dfd818e00e")
-BOT_TOKEN = config("BOT_TOKEN", default="7397532645:AAH7R46hDIQ95j9-5kyVgySCa-A6cHQy6vE")
-SESSION = config("SESSION", default="BQGPlygAI4BiNyrBdvwQZq0yotZUfdwMVAmeMl-x7w54XKZa4VrA1DZ6RBJk7ooZGXhh9AR6A9WLTEG61Cq9voBLqoAyGJqJMvudYb_jH3YH15z5X1KKGgkLHntgn_4u8JDQeN3-xNrz6ZHdR69NjGlWw-7DbwkfhFPnWKwP5VREi3R-74-6m3o3Pcyu1cSfZUD5Ve9R769-Gv2_93imHP3iyvNQ9CVtcHhVWjoavhWWF6xTLh3xEDbjqBjpZN9BoCK2aSnXKm7BBmwYsCjNsNe_0SpsEujeooQdAJH12XMv_s7RGrFWLf1X_asA35hZ6uM8Fo1-73tD-iMLfIScR0IaGynXKQAAAAGKe47tAA")
-FORCESUB = config("FORCESUB", default="movies0x1")
-AUTH = config("AUTH", default=6169288210, cast=int)
+
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
